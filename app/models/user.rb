889cb9fa3f90358a8813280b7655b7f125fd1ca4
avinash47 <avinash.vijayarangan@gmail.com>
2020-04-25 12:47:30 +0530

    Added BCrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     has_many :todos
+    has_secure_password
     def to_formatted_string
         "#{id}.  #{name} #{last_name} - #{email}-#{password}"
         end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_073900) do
+ActiveRecord::Schema.define(version: 2020_04_25_070851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(version: 2020_04_24_073900) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
     t.string "last_name"
+    t.string "password_digest"
   end
 
 end
